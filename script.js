359a7e579abbd81c8a4fb2dea3818e4b1ef3407b
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-03-10 21:32:30 -0500

    Cleanup, too

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// Add smooth scrolling to anchor links
-const cards = document.querySelectorAll('.card');
-
-// Function to check if an element is in view
-function isInView(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Function to handle the scroll event
-function handleScroll() {
-    cards.forEach(card => {
-        if (isInView(card)) {
-            card.classList.add('slide-in');
-        }
-    });
-}
-
-// Attach the scroll event listener to the window
-window.addEventListener('scroll', handleScroll);
-
-
 const typeWriter = document.getElementById('header-text');
 const nameText = 'Velvet Toroyashi';
 let i = 0;
@@ -40,12 +13,9 @@ function type() {
 
 window.addEventListener('load', type);
 
-const profileImage = document.querySelector('.profile-image');
 const headerText = document.querySelector('.header-text');
 
 window.addEventListener('load', () => {
 
     headerText.classList.add('animate');
-
 });
-
